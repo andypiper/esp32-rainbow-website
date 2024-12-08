6997c03f32f99a931413d7472f4d960b488bb751
Chris Greening <c_greening@apple.com>
2024-12-08 19:26:16 +0000

    optimisations

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,47 @@ import Machine from './pages/Debug'
 import Firmware from './pages/Firmware'
 import Docs from './pages/Docs'
 import GitHub from './pages/GitHub'
+import { Helmet } from 'react-helmet'
 
 function App() {
   return (
     <SerialProvider>
       <Router>
         <div className="min-h-screen bg-gray-900">
+          <Helmet>
+            <title>ESP32 Rainbow - LED Controller</title>
+            <meta name="description" content="Control your ESP32-powered LED strips with ease. Open-source hardware and software solution for creating beautiful LED lighting effects." />
+            
+            {/* Preconnect to external domains */}
+            <link rel="preconnect" href="https://www.youtube.com" />
+            <link rel="preconnect" href="https://www.crowdsupply.com" />
+            <link rel="preconnect" href="https://www.patreon.com" />
+
+            {/* JSON-LD structured data */}
+            <script type="application/ld+json">
+              {JSON.stringify({
+                "@context": "https://schema.org",
+                "@type": "Product",
+                "name": "ESP32 Rainbow LED Controller",
+                "description": "Control your ESP32-powered LED strips with ease. Open-source hardware and software solution for creating beautiful LED lighting effects.",
+                "brand": {
+                  "@type": "Brand",
+                  "name": "ESP32 Rainbow"
+                },
+                "offers": {
+                  "@type": "Offer",
+                  "url": "https://www.crowdsupply.com/atomic14/esp32-rainbow",
+                  "availability": "https://schema.org/InStock"
+                },
+                "url": "https://www.esp32rainbow.com",
+                "category": "Electronics",
+                "isAccessoryOrSparePartFor": {
+                  "@type": "Product",
+                  "name": "ESP32 Microcontroller"
+                }
+              })}
+            </script>
+          </Helmet>
           <Navbar />
           <main>
             <Routes>
