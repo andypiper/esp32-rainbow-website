4376077ab70b25c55b94b681bda1410bfb6b6e72
Chris Greening <c_greening@apple.com>
2024-12-08 19:31:45 +0000

    Fix typo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <img 
           src={ZXSpectrum48k} 
           alt="ZX Spectrum 48K" 
-          fetchpriority="high"
+          fetchPriority="high"
           className="w-full h-full object-cover object-center"
         />
         <div className="absolute inset-0 bg-gradient-to-b from-transparent to-gray-900"></div>
