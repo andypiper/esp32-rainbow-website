965053091f0d987ecba7d430024ef36dc815913b
Chris Greening <c_greening@apple.com>
2024-12-08 19:15:25 +0000

    make a nicer image

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import ZXSpectrum48k from '../assets/ZXSpectrum48k.jpg'
+import ZXSpectrum48k from '../assets/ZXSpectrum48k.png'
 
 export default function Home() {
   return (
